a0aae14c96c8c2b8f234f649be12e9389b940bad
Chris Kayahara <kayahara.christopher@gmail.com>
2023-03-21 17:21:31 -0400

    Comment code to run without variable imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
     </div>
   );
 }
