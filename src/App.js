2eabba95eced47b970180ea525647ff205ac8ce2
Chris Kayahara <kayahara.christopher@gmail.com>
2023-03-21 17:06:57 -0400

    Create header component structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header/Header';
+
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
