808b71be23a7fd0d7880375eadf9c6a12174f333
Chris Kayahara <kayahara.christopher@gmail.com>
2023-03-22 14:43:58 -0400

    Create footer component structure

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.scss'
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer_text">© InStock Inc. All Rights Reserved.</p>
+        </footer>
+    )
+}
