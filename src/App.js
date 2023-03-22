38f5c0fae516e82c083ba71d829ad26d7107dc42
Chris Kayahara <kayahara.christopher@gmail.com>
2023-03-22 15:32:33 -0400

    Create global styles file and reset margins, padding and border box sizing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import InventoryPage from './pages/InventoryPage/InventoryPage';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
+import './App.scss'
 
 function App() {
   return (
