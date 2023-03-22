b6898b978ae86e4d0dc66601f69ac78e5ac80601
Chris Kayahara <kayahara.christopher@gmail.com>
2023-03-22 15:21:43 -0400

    Finalize footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.scss'
 export default function Footer() {
     return (
         <footer className="footer">
-            <p className="footer_text">© InStock Inc. All Rights Reserved.</p>
+            <p className="footer__text">© InStock Inc. All Rights Reserved.</p>
         </footer>
     )
 }
