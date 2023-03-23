bc35ec63ae3d5a6771eeb24ab60d5642f282e5b3
Chris Kayahara <kayahara.christopher@gmail.com>
2023-03-23 14:07:04 -0400

    Created Add Inventory Page component with responsive styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import WarehousesPage from './pages/WarehousesPage/WarehousesPage';
 import InventoryPage from './pages/InventoryPage/InventoryPage';
 import AddWarehousePage from './pages/AddWarehousePage/AddWarehousePage';
+import AddInventoryPage from './pages/AddInventoryPage/AddInventoryPage';
 import EditWarehousePage from './pages/EditWarehousePage/EditWarehousePage';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -18,6 +19,7 @@ function App() {
    <Routes>
        <Route path='/' element={<WarehousesPage/>}></Route>
         <Route path='/inventory' element={<InventoryPage/>}></Route>
+        <Route path='/inventory/add' element={<AddInventoryPage/>}></Route>
         <Route path='*' element={<NotFoundPage/>}></Route>
         <Route path='/warehouse/add' element={<AddWarehousePage/>}></Route>
         <Route path='/warehouse/edit/:id' element={<EditWarehousePage/>}></Route>
