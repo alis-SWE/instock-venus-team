c0dd8a1a9fe2b1fec1536c4d82be9bdbd19dc17b
Ali <ali.cs.sheikh@gmail.com>
2023-03-24 02:06:37 -0400

    fix bug

--- src/components/DeleteWarehouse/DeleteWarehouse.jsx
+++ src/components/DeleteWarehouse/DeleteWarehouse.jsx
@@ -1,5 +1,48 @@
-const DeleteButton = () => {
-    return (  );
+import axios from 'axios';
+import close from '../../assets/icons/close-24px.svg';
+import './DeleteWarehouse.scss';
+
+const DeleteWarehouse = ({ closeModal, id }) => {
+
+    const handleClick = () => {
+        closeModal(false);
+        axios.delete('http://localhost:8080/warehouse/' + id)
+            .then((response) => {
+                console.log('Warehouse deleted: ', response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        
+    }
+
+    return ( 
+        <div className="modalBackground">
+            <div className="modalContainer">
+            <img src={close} onClick= {() => closeModal(false)} alt="" />
+    
+            <div className="title"> 
+                <h1>Delete Washington warehouse?</h1>
+            </div>
+    
+            <div className="body">
+                <p>Please confirm that you;d like to delete the Washington from the list of warehouses.
+                    You won't be able to undo this action.
+                </p>
+            </div>
+            <div className="footer">
+            <button className="cancel-button" onClick= {() => closeModal(false)}>
+                <p className="cancel-button__text" >Cancel</p>
+            </button>
+                
+    
+            <button className="delete-button" onClick= {handleClick}>
+                <p className="delete-button__text" >Delete</p>
+            </button>
+            </div>
+            </div>
+      </div>
+     );
 }
  
-export default DeleteButton;
+export default DeleteWarehouse;
--- src/components/WarehouseCard/WarehouseCard.jsx
+++ src/components/WarehouseCard/WarehouseCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import "./WarehouseCard.scss"
+import DeleteWarehouse from '../DeleteWarehouse/DeleteWarehouse'
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg"
 import editIcon from "../../assets/icons/edit-24px.svg"
 import arrow from "../../assets/icons/chevron_right-24px.svg"
+import { useState } from 'react'
 
 export default function WarehouseCard( { warehouse, address, city, country,contactName, contactPhone, contactEmail ,id }) {
+    const [openModal, setOpenModal] = useState(false);
     return (
         <div className="warehouse">
             <div className="warehouse__col-1"> 
@@ -20,9 +23,7 @@ export default function WarehouseCard( { warehouse, address, city, country,conta
                     <p className="warehouse__label">ADDRESS</p>
                     <p>{`${address}, ${city}, ${country}`}</p>
                 </div>
-                <Link className="warehouse__delete">
-                    <img src={deleteIcon} alt=""/>
-                </Link>
+                    <img src={deleteIcon} alt="" className='warehouse__delete' onClick={() => {setOpenModal(true)}}/>
             </div>
             <div className="warehouse__col-2"> 
                 <div className="warehouse__name-container">
@@ -39,13 +40,14 @@ export default function WarehouseCard( { warehouse, address, city, country,conta
                 </Link>
             </div>
             <div className="warehouse__actions-container">
-                <Link className="warehouse__delete--tablet">
-                    <img src={deleteIcon} alt="delete icon"/>
-                </Link>
+                <img src={deleteIcon} alt="" className='warehouse__delete--tablet' onClick={() => {setOpenModal(true)}}/>
+                {/* {openModal && <DeleteWarehouse closeModal={setOpenModal}/>} */}
                 <Link className="warehouse__edit--tablet">
                     <img src={editIcon} alt="edit icon"/>
                 </Link>
             </div>
+            {openModal && <DeleteWarehouse closeModal={setOpenModal}/>}
+
 
         </div>
     )
